d5dcddd2098ad64bfa82ca4eaf23133411ea589b
SANSA HAMDI <sansa.hamdi@gmail.com>
2021-04-05 17:16:11 +0200

    nodemailer

--- back-end/utils/sendEmail.js
+++ back-end/utils/sendEmail.js
@@ -1,24 +1,33 @@
 const nodemailer = require("nodemailer");
-const { options } = require("yargs");
 
-const sendEmail = async (options) => {
-  const transporter = nodemailer.createTransport({
-    host: process.env.SMTP_HOST,
-    port: process.env.SMTP_PORT,
+const sendEmail = (option) => {
+  let transporter = nodemailer.createTransport({
+    service: "gmail",
     auth: {
       user: process.env.SMTP_EMAIL,
       pass: process.env.SMTP_PASSWORD,
     },
   });
-
-  const message = {
-    from: `${process.env.SMTP_FROM_NAME} <${process.env.SMTP_FROM_EMAIL}>`,
-    to: options.email,
-    subject: options.subject,
-    text: options.message,
+  const mailOptions = {
+    from: process.env.SMTP_EMAIL,
+    to: option.email,
+    subject: option.subject,
+    html: `
+    <div>
+    <h1>Don't miss your code</h1>
+      <ul>
+        <li style={{listDecoration:'none'}}> user :  ${option.email} </li>
+        <h3> ${option.message}</h3>
+      </ul>
+    </div>
+    `,
   };
 
-  await transporter.sendMail(message);
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    }
+  });
 };
 
 module.exports = sendEmail;
